c22484afefade5336deba2cc8128fa01b4a001ce
lawrence910426 <lawrence910426@gmail.com>
2021-09-24 03:43:20 -0700

    Initialize Project Structure

---
+++ Lab1-Advanced/Lab1-Advanced/Lab1-Advanced.srcs/sim_1/new/dmux_tb.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2021 03:40:55 AM
+// Design Name: 
+// Module Name: dmux_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dmux_tb(
+
+    );
+    
+    dmux verify();
+endmodule
---
+++ Lab1-Advanced/Lab1-Advanced/Lab1-Advanced.srcs/sources_1/new/dmux.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2021 03:40:35 AM
+// Design Name: 
+// Module Name: dmux
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dmux(
+
+    );
+endmodule
