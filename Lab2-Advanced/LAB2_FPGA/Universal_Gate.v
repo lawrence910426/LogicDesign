539ea9b14a63030b0230030e71db62b917c59021
TACO1031 <75667026+TACO1031@users.noreply.github.com>
2021-10-14 14:52:41 +0800

    FPGA!!!!DONE~

---
+++ Lab2-Advanced/LAB2_FPGA/Universal_Gate.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/10/11 19:25:07
+// Design Name: 
+// Module Name: Universal_Gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Universal_Gate(a, b, out);
+    input a, b;
+    output out;
+    wire b_n;
+    
+    not n1(b_n, b);
+    and a1(out, b_n, a);
+    
+endmodule
