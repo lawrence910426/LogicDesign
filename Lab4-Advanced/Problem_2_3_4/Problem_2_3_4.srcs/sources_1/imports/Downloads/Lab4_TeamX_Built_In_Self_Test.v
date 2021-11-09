cbf1a0c28e99e2b3b078789d2684939faced920e
lawrence910426 <lawrence910426@gmail.com>
2021-11-09 15:02:12 +0800

    Import Basic Lab for compilation

--- Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/Lab4_TeamX_Built_In_Self_Test.v
+++ Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/Lab4_TeamX_Built_In_Self_Test.v
@@ -14,7 +14,21 @@ module Built_In_Self_Test(clk, rst_n, scan_en, scan_in, scan_out);
     );
 endmodule
 
-`timescale 1ns/1ps
+module Many_To_One_LFSR(clk, rst_n, out);
+    input clk;
+    input rst_n;
+    output reg [7:0] out;
+    
+    always @ (posedge clk) begin
+        if (rst_n == 1'b0) begin
+            out <= 8'b10111101;
+        end else begin
+            out[7:1] <= out[6:0];
+            out[0] <= out[1] ^ out[2] ^ out[3] ^ out[7];
+        end
+    end
+endmodule
+
 
 module Scan_Chain_Design(clk, rst_n, scan_in, scan_en, scan_out);
     input clk;
