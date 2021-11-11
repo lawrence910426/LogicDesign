452fa58d5a1942ed5632fa4457f7f13b96daa7b4
u109062328 <u109062328@ic58.cs.nthu.edu.tw>
2021-11-11 16:02:50 +0800

    Unsynthesisable RTL code fix

---
+++ Lab4-Advanced/Problem_1/THINGS/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,1 @@
+1636617668 /users/course/2021F/ldl12200000/u109062328/LogicDesign/Lab4-Advanced/Problem_1/THINGS/Lab4_Team3_Content_Addressable_Memory.v
---
+++ Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,3 @@
+1636441120 /users/course/2021F/ldl12200000/u109062328/LogicDesign/Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/Lab4_TeamX_Scan_Chain_Design.v
+1636441120 /users/course/2021F/ldl12200000/u109062328/LogicDesign/Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/Lab4_TeamX_Mealy_Sequence_Detector.v
+1636441344 /users/course/2021F/ldl12200000/u109062328/LogicDesign/Lab4-Advanced/Problem_2_3_4/Problem_2_3_4.srcs/sources_1/imports/Downloads/Lab4_TeamX_Built_In_Self_Test.v
