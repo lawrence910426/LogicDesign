59248fef85a64d2950c805081b268b63021ef492
lawrence910426 <lawrence910426@gmail.com>
2022-01-13 17:53:45 +0800

    Reform files due to version control issues
    Fuck you git.

--- Final/CNN/CNN.srcs/sources_1/imports/Users/lawre/Downloads/CNN-FPGA-master/CNN-FPGA-master/src/ConvKernel.v
+++ Final/CNN/CNN.srcs/sources_1/imports/Users/lawre/Downloads/CNN-FPGA-master/CNN-FPGA-master/src/ConvKernel.v
@@ -31,7 +31,8 @@ module ConvKernel#(
     input start,
     input clk,
     output [BITWIDTH - 1 : 0] result,
-    output finish
+    output finish,
+    output [1024 - 1 : 0] debug
 );
     wire [BITWIDTH - 1 : 0] data_wire [DATACHANNEL * 2 * 2 - 1:0];
     wire [BITWIDTH - 1 : 0] weight_wire [DATACHANNEL * 2 * 2 - 1:0];
@@ -45,7 +46,7 @@ module ConvKernel#(
     
     reg [BITWIDTH - 1:0] Sum, Increment, X, Y;
     reg [10 - 1:0] Latency;
-    reg [DATACHANNEL - 1:0] i;
+    reg [DATACHANNEL + 3 - 1:0] i;
     reg Finish;
     
     wire [BITWIDTH - 1:0] Sum_Output, Z;
@@ -59,7 +60,7 @@ module ConvKernel#(
     
     always @ (posedge clk) begin
         if (start == 1'b1) begin
-            Sum <= 0;
+            Sum <= bias;
             Increment <= 0;
             X <= 0;
             Y <= 0;
@@ -74,14 +75,6 @@ module ConvKernel#(
             Latency <= 0;
             i <= 0;
             Finish <= 1'b1;
-        end else if (Latency == LATENCY_MAX) begin
-            Sum <= Sum_Output;
-            Increment <= Z;
-            i <= i + 1;
-            Latency <= 0;
-            X <= data_wire[i];
-            Y <= weight_wire[i];
-            Finish <= 1'b0;
         end else if (i == DATACHANNEL * 2 * 2) begin
             Sum <= Sum;
             Increment <= Increment;
@@ -90,6 +83,14 @@ module ConvKernel#(
             i <= i;
             Latency <= Latency + 1;
             Finish <= 1'b1;
+        end else if (Latency == LATENCY_MAX) begin
+            Sum <= Sum_Output;
+            Increment <= Z;
+            i <= i + 1;
+            Latency <= 0;
+            X <= data_wire[i];
+            Y <= weight_wire[i];
+            Finish <= 1'b0;
         end else begin
             Sum <= Sum;
             Increment <= Increment;
