8205ff59f93ffaf6a3d139cb157c23e5372498d1
lawrence910426 <lawrence910426@gmail.com>
2022-01-14 01:33:15 +0800

    Lose weight on BRAM

--- Final/basys3_ov7670_v1/basys3_ov7670_v1.ip_user_files/ip/frame_buffer/sim/frame_buffer.v
+++ Final/basys3_ov7670_v1/basys3_ov7670_v1.ip_user_files/ip/frame_buffer/sim/frame_buffer.v
@@ -1,4 +1,4 @@
-// (c) Copyright 1995-2018 Xilinx, Inc. All rights reserved.
+// (c) Copyright 1995-2022 Xilinx, Inc. All rights reserved.
 // 
 // This file contains confidential and proprietary information
 // of Xilinx, Inc. and is protected under U.S. and
@@ -48,7 +48,7 @@
 
 
 // IP VLNV: xilinx.com:ip:blk_mem_gen:8.4
-// IP Revision: 0
+// IP Revision: 4
 
 `timescale 1ns/1ps
 
@@ -68,19 +68,19 @@ input wire clka;
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTA WE" *)
 input wire [0 : 0] wea;
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTA ADDR" *)
-input wire [16 : 0] addra;
-(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME BRAM_PORTA, MEM_SIZE 8192, MEM_WIDTH 32, MEM_ECC NONE, MASTER_TYPE OTHER, READ_WRITE_MODE READ_WRITE" *)
+input wire [14 : 0] addra;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME BRAM_PORTA, MEM_SIZE 8192, MEM_WIDTH 32, MEM_ECC NONE, MASTER_TYPE OTHER, READ_LATENCY 1" *)
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTA DIN" *)
 input wire [11 : 0] dina;
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTB CLK" *)
 input wire clkb;
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTB ADDR" *)
-input wire [16 : 0] addrb;
-(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME BRAM_PORTB, MEM_SIZE 8192, MEM_WIDTH 32, MEM_ECC NONE, MASTER_TYPE OTHER, READ_WRITE_MODE READ_WRITE" *)
+input wire [14 : 0] addrb;
+(* X_INTERFACE_PARAMETER = "XIL_INTERFACENAME BRAM_PORTB, MEM_SIZE 8192, MEM_WIDTH 32, MEM_ECC NONE, MASTER_TYPE OTHER, READ_LATENCY 1" *)
 (* X_INTERFACE_INFO = "xilinx.com:interface:bram:1.0 BRAM_PORTB DOUT" *)
 output wire [11 : 0] doutb;
 
-  blk_mem_gen_v8_4_0 #(
+  blk_mem_gen_v8_4_4 #(
     .C_FAMILY("artix7"),
     .C_XDEVICEFAMILY("artix7"),
     .C_ELABORATION_DIR("./"),
@@ -112,9 +112,9 @@ output wire [11 : 0] doutb;
     .C_WRITE_MODE_A("NO_CHANGE"),
     .C_WRITE_WIDTH_A(12),
     .C_READ_WIDTH_A(12),
-    .C_WRITE_DEPTH_A(131072),
-    .C_READ_DEPTH_A(131072),
-    .C_ADDRA_WIDTH(17),
+    .C_WRITE_DEPTH_A(32768),
+    .C_READ_DEPTH_A(32768),
+    .C_ADDRA_WIDTH(15),
     .C_HAS_RSTB(0),
     .C_RST_PRIORITY_B("CE"),
     .C_RSTRAM_B(0),
@@ -126,9 +126,9 @@ output wire [11 : 0] doutb;
     .C_WRITE_MODE_B("WRITE_FIRST"),
     .C_WRITE_WIDTH_B(12),
     .C_READ_WIDTH_B(12),
-    .C_WRITE_DEPTH_B(131072),
-    .C_READ_DEPTH_B(131072),
-    .C_ADDRB_WIDTH(17),
+    .C_WRITE_DEPTH_B(32768),
+    .C_READ_DEPTH_B(32768),
+    .C_ADDRB_WIDTH(15),
     .C_HAS_MEM_OUTPUT_REGS_A(0),
     .C_HAS_MEM_OUTPUT_REGS_B(1),
     .C_HAS_MUX_OUTPUT_REGS_A(0),
@@ -139,6 +139,8 @@ output wire [11 : 0] doutb;
     .C_USE_SOFTECC(0),
     .C_USE_ECC(0),
     .C_EN_ECC_PIPE(0),
+    .C_READ_LATENCY_A(1),
+    .C_READ_LATENCY_B(1),
     .C_HAS_INJECTERR(0),
     .C_SIM_COLLISION_CHECK("ALL"),
     .C_COMMON_CLK(0),
@@ -151,9 +153,9 @@ output wire [11 : 0] doutb;
     .C_EN_SHUTDOWN_PIN(0),
     .C_EN_SAFETY_CKT(0),
     .C_DISABLE_WARN_BHV_RANGE(0),
-    .C_COUNT_36K_BRAM("44"),
+    .C_COUNT_36K_BRAM("11"),
     .C_COUNT_18K_BRAM("0"),
-    .C_EST_POWER_SUMMARY("Estimated Power for IP     :     17.081226 mW")
+    .C_EST_POWER_SUMMARY("Estimated Power for IP     :     17.470026 mW")
   ) inst (
     .clka(clka),
     .rsta(1'D0),
--- Final/basys3_ov7670_v1/basys3_ov7670_v1.ip_user_files/sim_scripts/frame_buffer/questa/compile.do
+++ Final/basys3_ov7670_v1/basys3_ov7670_v1.ip_user_files/sim_scripts/frame_buffer/questa/compile.do
@@ -1,26 +1,26 @@
 vlib questa_lib/work
 vlib questa_lib/msim
 
-vlib questa_lib/msim/xil_defaultlib
 vlib questa_lib/msim/xpm
-vlib questa_lib/msim/blk_mem_gen_v8_4_0
+vlib questa_lib/msim/blk_mem_gen_v8_4_4
+vlib questa_lib/msim/xil_defaultlib
 
-vmap xil_defaultlib questa_lib/msim/xil_defaultlib
 vmap xpm questa_lib/msim/xpm
-vmap blk_mem_gen_v8_4_0 questa_lib/msim/blk_mem_gen_v8_4_0
+vmap blk_mem_gen_v8_4_4 questa_lib/msim/blk_mem_gen_v8_4_4
+vmap xil_defaultlib questa_lib/msim/xil_defaultlib
 
-vlog -work xil_defaultlib -64 -sv \
-"C:/Xilinx/Vivado/2017.3/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
-"C:/Xilinx/Vivado/2017.3/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+vlog -work xpm  -sv \
+"C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
 
-vcom -work xpm -64 -93 \
-"C:/Xilinx/Vivado/2017.3/data/ip/xpm/xpm_VCOMP.vhd" \
+vcom -work xpm  -93 \
+"C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_VCOMP.vhd" \
 
-vlog -work blk_mem_gen_v8_4_0 -64 \
+vlog -work blk_mem_gen_v8_4_4  \
 "../../../ipstatic/simulation/blk_mem_gen_v8_4.v" \
 
-vlog -work xil_defaultlib -64 \
+vlog -work xil_defaultlib  \
 "../../../ip/frame_buffer/sim/frame_buffer.v" \
+
 
 vlog -work xil_defaultlib \
 "glbl.v"
